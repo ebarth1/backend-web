7cca42e7099ede33901cf35f096a553916ccb6d1
ebarth1 <ebarth.github@gmail.com>
2024-11-21 19:27:10 +0100

    first endpoint

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Resources\UserResource;
+use App\Models\User;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('users', function () {
+    return UserResource::collection(User::all());
 });
